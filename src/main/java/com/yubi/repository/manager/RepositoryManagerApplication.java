cf19d297c01462983dde692bb242e472f2675b9e
Dharmesh Ravichandran <51646318+rrdharmesh@users.noreply.github.com>
2025-04-09 15:15:58 +0530

    Add main application class

---
+++ src/main/java/com/yubi/repository/manager/RepositoryManagerApplication.java
@@ -0,0 +1,11 @@
+package com.yubi.repository.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RepositoryManagerApplication.class, args);
+    }
+}
